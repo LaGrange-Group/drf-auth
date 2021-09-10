2404be32541c27bc7078375a74228a26d77d88b7
David La Grange <davidnlagrange@hotmail.com>
2021-09-10 13:57:32 -0500

    Changed App Name to 'Authentication' + Completed view.py Registration View Function + Tested Working in Postman

--- auth_project/authentication/views.py
+++ auth_project/authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 
 class RegisterView(generics.CreateAPIView):
